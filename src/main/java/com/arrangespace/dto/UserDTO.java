b2dec4f9a8f34b5e111f95c91dd94830149e886f
Jeff Chiu <jeffchiu2000@gmail.com>
2019-09-18 18:46:53 -0700

    Java formatting + user API

---
+++ src/main/java/com/arrangespace/dto/UserDTO.java
@@ -0,0 +1,17 @@
+package com.arrangespace.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserDTO {
+
+	private String familyName;
+	private String imageUrl;
+	private String name;
+	private String googleId;
+	private String email;
+	private String givenName;
+
+}
